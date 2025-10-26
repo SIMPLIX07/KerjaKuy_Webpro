f609eb2f21e662ef0d422fcfe9c1760a4e6710fc
SIMPLIX07 <muhammad99salman98@gmail.com>
2025-10-26 20:41:14 +0700

    js

---
+++ pageLamar/lamar.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const buttonLamar = document.querySelector('.button');
+
+    buttonLamar.addEventListener('click', function () {
+        alert("Lamaran Anda sudah dikirim");
+    });
+});
