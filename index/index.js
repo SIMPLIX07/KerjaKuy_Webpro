d6a20aedf9ba009a263d911318897136d5ec92f5
SIMPLIX07 <muhammad99salman98@gmail.com>
2025-11-02 23:24:57 +0700

    memperbaiki button login dan daftar di index

--- index/index.js
+++ index/index.js
@@ -1,26 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const btnLogin = document.getElementById('login');
-    const btnSign = document.getElementById('daftar');
+document.getElementById("pelamar").onclick = function () {
+  window.location.href = "../LoginPelamar/Login.html";
+};
 
-    btnLogin.addEventListener('click', async function () {
-        let userConfirm = confirm("Login Sebagai Perusahaan/User");
+document.getElementById("perusahaan").onclick = function () {
+  window.location.href = "../LoginPerusahaan/Login.html";
+};
 
-        if (userConfirm){
-            window.location.href = "../LoginPelamar/login.html"
-        }else{
-            window.location.href = "../LoginPerusahaan/login.html"
-        }
+document.getElementById("daftar-pelamar").onclick = function () {
+  window.location.href = "../SignupPelamar/signPelamar.html";
+};
 
-    });
-    btnSign.addEventListener('click', async function () {
-        let userConfirm = confirm("Sign Up Sebagai Perusahaan/User");
+document.getElementById("daftar-perusahaan").onclick = function () {
+  window.location.href = "../SignupPerusahaan/signPerusahaan.html";
+};
 
-        if (userConfirm){
-            window.location.href = "../SignupPelamar/signPelamar.html"
-        }else{
-            window.location.href = "../LoginPerusahaan/login.html"
-        }
-
-    });
-    
-});
